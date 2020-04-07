7a114c9e3b9a484309abd33eb382e5b96aca6df9
tghgg <falling_snowdin@protonmail.com>
2020-04-08 02:41:39 +0700

    Update setup script and add a cron job

--- setup_script.sh
+++ setup_script.sh
@@ -1,66 +1,96 @@
 #!/bin/bash
 
-# Determine the package manager of the system
-echo "What package manager is the current environment using?"
-read package_manager
-echo "You are using $package_manager. Installing your packages..."
+echo "What do you want to do?"
+echo 
+echo "install-base-packages"
+echo "install-joplin"
+echo "install-unity"
+echo "download-github-projects"
+echo "configure-tlp-powertop"
+echo "all"
+echo
+read action
 
-# Install packages I need
+case "$action" in
+"install-base-packages")
+    # Determine the package manager of the system
+    echo "What package manager is the current environment using?"
+    read package_manager
+    echo "You are using $package_manager. Installing your packages..."
 
-case "$package_manager" in
-# Ubuntu/Debian
-"apt")
-    sudo apt install neofetch tlp powertop godot3
-    sudo snap install discord
-    sudo snap install codium --classic
-    ;;
-# Arch
-"pacman")
-    sudo pacman -S neofetch tlp powertop pacmatic discord papirus-icon-theme breeze fcitx fcitx-unikey kcm-fcitx
-    sudo pacmatic -Syu
-    echo "Choose the Desktop Environment you want to use."
-    read DE
-    echo "Installing $DE"
-    case "$DE" in
-    "plasma")
-        sudo pacmatic -S plasma-meta kate konsole dolphin
+    # Install packages I need
+
+    case "$package_manager" in
+    # Ubuntu/Debian
+    "apt")
+        sudo apt install neofetch tlp powertop godot3
+        sudo snap install discord
+        sudo snap install codium --classic
         ;;
-    "xfce")
-        sudo pacmatic -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+    # Arch
+    "pacman")
+        sudo pacman -S neofetch tlp powertop pacmatic discord papirus-icon-theme breeze fcitx fcitx-unikey kcm-fcitx
+        sudo pacmatic -Syu
+        echo "Choose the Desktop Environment you want to use."
+        read DE
+        echo "Installing $DE"
+        case "$DE" in
+        "plasma")
+            sudo pacmatic -S plasma-meta kate konsole dolphin
+            ;;
+        "xfce")
+            sudo pacmatic -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+            ;;
+        *)
+            echo "DE choice unrecognized. Moving on...."
+            ;;
+        esac
+        ;;
+    # Fedora
+    "dnf")
+        sudo dnf install neofetch tlp powertop godot
+        ;;
+    # openSUSE
+    "zypper")
+        sudo zypper install neofetch tlp powertop
         ;;
     *)
-        echo "DE choice unrecognized. Moving on...."
+        echo "Package manager not recognized. Stopping..."
         ;;
     esac
     ;;
-# Fedora
-"dnf")
-    sudo dnf install neofetch tlp powertop godot
+"install-joplin")
+    # Install Joplin with their script
+    echo "Installing Joplin..."
+    wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
     ;;
-# openSUSE
-"zypper")
-    sudo zypper install neofetch tlp powertop
+"install-unity")
+    # Download Unity Hub
+    echo "Downloading Unity..."
+    cd ~/Desktop
+    wget -O UnityHub.AppImage https://public-cdn.cloud.unity3d.com/hub/prod/UnityHub.AppImage
+    chmod +x UnityHub.AppImage
+    ;;
+"configure-tlp-powertop")
+    # Configure stuff
+    echo "Enabling TLP and auto-tuning Powertop..."
+    sudo systemctl enable tlp
+    sudo systemctl start tlp
+    sudo powertop --auto-tune
+    ;;
+"download-github-projects")
+    # Clone my code projects from GitHub
+    echo "Cloning your projects from GitHub..."
+    mkdir ~/Code
+    cd ~/Code
+    git clone https://github.com/tghgg/SeekerGame.git
+    git clone https://github.com/tghgg/AWoO.git
+    git clone https://github.com/tghgg/music-player.git
     ;;
 *)
-    echo "Package manager not recognized. Stopping..."
+    echo "Action unrecognized."
     ;;
 esac
-# Install Joplin with their script
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
-
-# Configure stuff
-sudo systemctl enable tlp
-sudo systemctl start tlp
-sudo powertop --auto-tune
-
-
-# Clone my code projects from GitHub
-mkdir ~/Code
-cd ~/Code
-git clone https://github.com/tghgg/SeekerGame.git
-git clone https://github.com/tghgg/AWoO.git
-git clone https://github.com/tghgg/music-player.git
-
 # Cleanup and finish
 clear
-neofetch
+echo "Done!"
